444c10241195302641f953e62a682dd3051b1747
谭源 <kentertan12138@Outlook.com>
2023-03-19 19:43:57 +0800

    add sbi stuff

--- include/sys/types.h
+++ include/sys/types.h
@@ -32,6 +32,15 @@ typedef int daddr_t;
 typedef long off_t;
 typedef unsigned char u_char;
 typedef unsigned short ushort;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
+typedef unsigned short uint16_t;
+typedef short int16_t;
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+typedef signed long long ssize_t;
 
 typedef struct { int quot,rem; } div_t;
 typedef struct { long quot,rem; } ldiv_t;
